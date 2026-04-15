ed682939c61b8e77ed787af509fddf7e3083790f
bolaji <bolajiajakaiye@gmail.com>
2026-04-15 11:17:36 +0100

    fix:fixed bug

--- src/calculator.js
+++ src/calculator.js
@@ -1,5 +1,5 @@
 // src/calculator.js
-function add(a, b)      { return a - b; }
+function add(a, b)      { return a + b; }
 function subtract(a, b) { return a - b; }
 function multiply(a, b) { return a * b; }
 function divide(a, b) {
